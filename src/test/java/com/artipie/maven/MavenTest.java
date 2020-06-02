b5e6129071cde4939b47ebc9eaf49f138435a9f2
paulodamaso <pauloeduardolobo@gmail.com>
2020-06-02 11:28:32 -0300

    For #91: added unsupported checksum algorithm test

--- src/test/java/com/artipie/maven/MavenTest.java
+++ src/test/java/com/artipie/maven/MavenTest.java
@@ -24,6 +24,7 @@
 
 package com.artipie.maven;
 
+import com.artipie.asto.Content;
 import com.artipie.asto.Key;
 import com.artipie.asto.memory.InMemoryStorage;
 import org.hamcrest.MatcherAssert;
@@ -53,6 +54,11 @@ public class MavenTest {
      * Maven name.
      */
     public static final String MAVEN = "maven";
+
+    /**
+     * Unsupported algorithm checksum file name.
+     */
+    public static final String UNSUPPORTED = "maven-metadata.xml.unsupported";
 
     @Test
     @Disabled
@@ -141,6 +147,33 @@ public class MavenTest {
                 )
             ),
             new IsEqual<>(true)
+        );
+    }
+
+    @Test
+    @Disabled
+    public void removeUnsupportedAlgorithmChecksum() throws Exception {
+        final InMemoryStorage storage = new InMemoryStorage();
+        storage.save(
+            new Key.From(
+                MavenTest.COM, MavenTest.ARTIPIE, MavenTest.MAVEN, MavenTest.UNSUPPORTED
+            ),
+            new Content.From("anything".getBytes())
+        );
+        new Maven(storage)
+            .update(new Key.From(MavenTest.COM, MavenTest.ARTIPIE, MavenTest.MAVEN))
+            .toCompletableFuture()
+            .get();
+        MatcherAssert.assertThat(
+            storage.exists(
+                new Key.From(
+                    MavenTest.COM,
+                    MavenTest.ARTIPIE,
+                    MavenTest.MAVEN,
+                    MavenTest.UNSUPPORTED
+                )
+            ),
+            new IsEqual<>(false)
         );
     }
 }
