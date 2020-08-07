deb1f06636bb3692abb28a1d28e12be46c6df702
Kirill <g4s8.public@gmail.com>
2020-08-07 17:24:32 +0300

    #117 - (cr) added todo for tests

--- src/main/java/com/artipie/maven/http/LocalMavenSlice.java
+++ src/main/java/com/artipie/maven/http/LocalMavenSlice.java
@@ -53,6 +53,9 @@ import org.reactivestreams.Publisher;
  *
  * @see Repository
  * @since 0.5
+ * @todo #117:30min Add test to verify this class.
+ *  Create integration test against local maven repository to download artifacts from
+ *  Artipie Maven repository and verify that all HEAD and GET requests has correct headers.
  * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
  */
 final class LocalMavenSlice implements Slice {
