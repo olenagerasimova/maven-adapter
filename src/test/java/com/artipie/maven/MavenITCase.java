92c28121a2cea9a048b8773f11bf9e944f6e117f
olenagerasimova <olena.gerasimova@gmail.com>
2020-09-04 13:29:33 +0300

    #137 - added tests with auth and deploy

--- src/test/java/com/artipie/maven/MavenITCase.java
+++ src/test/java/com/artipie/maven/MavenITCase.java
@@ -29,6 +29,10 @@ import com.artipie.asto.blocking.BlockingStorage;
 import com.artipie.asto.fs.FileStorage;
 import com.artipie.asto.memory.InMemoryStorage;
 import com.artipie.asto.test.TestResource;
+import com.artipie.http.auth.Authentication;
+import com.artipie.http.auth.BasicIdentities;
+import com.artipie.http.auth.Identities;
+import com.artipie.http.auth.Permissions;
 import com.artipie.http.slice.LoggingSlice;
 import com.artipie.maven.http.MavenSlice;
 import com.artipie.vertx.VertxSliceServer;
@@ -36,20 +40,26 @@ import io.vertx.reactivex.core.Vertx;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.cactoos.list.ListOf;
 import org.cactoos.scalar.Unchecked;
 import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.hamcrest.core.StringContains;
 import org.hamcrest.text.StringContainsInOrder;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.testcontainers.Testcontainers;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
 
 /**
  * Maven integration test.
@@ -64,6 +74,11 @@ public final class MavenITCase {
      * Vertx instance.
      */
     private static final Vertx VERTX = Vertx.vertx();
+
+    /**
+     * Test user.
+     */
+    private static final Pair<String, String> USER = new ImmutablePair<>("Alladin", "openSesame");
 
     /**
      * Temporary directory for all tests.
@@ -92,28 +107,11 @@ public final class MavenITCase {
      */
     private int port;
 
-    @BeforeEach
-    void init() throws IOException, InterruptedException {
-        this.storage = new InMemoryStorage();
-        this.server = new VertxSliceServer(
-            MavenITCase.VERTX,
-            new LoggingSlice(new MavenSlice(this.storage))
-        );
-        this.port = this.server.start();
-        Testcontainers.exposeHostPorts(this.port);
-        final Path setting = this.tmp.resolve("settings.xml");
-        setting.toFile().createNewFile();
-        Files.write(setting, this.settings());
-        this.cntn = new GenericContainer<>("centos:centos8")
-            .withCommand("tail", "-f", "/dev/null")
-            .withWorkingDirectory("/home/")
-            .withFileSystemBind(this.tmp.toString(), "/home");
-        this.cntn.start();
-        this.cntn.execInContainer("yum", "-y", "install", "maven");
-    }
-
-    @Test
-    void downloadsDependency() throws Exception {
+    @ParameterizedTest
+    @ValueSource(strings = {"all", "anonymous"})
+    void downloadsDependency(final String type) throws Exception {
+        this.init(this.auth(type));
+        this.settings(this.getUser(type));
         this.addFilesToStorage();
         MatcherAssert.assertThat(
             this.exec(
@@ -130,6 +128,40 @@ public final class MavenITCase {
         );
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"all", "anonymous"})
+    void deploysArtifact(final String type) throws Exception {
+        this.init(this.auth(type));
+        this.settings(this.getUser(type));
+        FileUtils.copyDirectory(
+            new TestResource("helloworld-src").asPath().toFile(),
+            this.tmp.resolve("helloworld-src").toFile()
+        );
+        Files.write(
+            this.tmp.resolve("helloworld-src/pom.xml"),
+            String.format(
+                Files.readString(this.tmp.resolve("helloworld-src/pom.xml")), this.port
+            ).getBytes()
+        );
+        MatcherAssert.assertThat(
+            "Build success",
+            this.exec(
+                "mvn", "-s", "/home/settings.xml", "-f", "/home/helloworld-src/pom.xml", "deploy"
+            ).replaceAll("\n", ""),
+            new StringContains("BUILD SUCCESS")
+        );
+        MatcherAssert.assertThat(
+            "Artifacts added to storage",
+            this.storage.list(new Key.From("com/artipie/helloworld"))
+                .join().stream().map(Key::string).collect(Collectors.toList()),
+            Matchers.hasItems(
+                "com/artipie/helloworld/maven-metadata.xml",
+                "com/artipie/helloworld/1.0/helloworld-1.0.pom",
+                "com/artipie/helloworld/1.0/helloworld-1.0.jar"
+            )
+        );
+    }
+
     @AfterEach
     void stopContainer() {
         this.server.close();
@@ -141,34 +173,61 @@ public final class MavenITCase {
         MavenITCase.VERTX.close();
     }
 
-    /**
-     * Executes dnf command in container.
-     * @param actions What to do
-     * @return String stdout
-     * @throws Exception On error
-     */
+    void init(final Pair<Permissions, Identities> auth) throws IOException,
+        InterruptedException {
+        this.storage = new InMemoryStorage();
+        this.server = new VertxSliceServer(
+            MavenITCase.VERTX,
+            new LoggingSlice(new MavenSlice(this.storage, auth.getKey(), auth.getValue()))
+        );
+        this.port = this.server.start();
+        Testcontainers.exposeHostPorts(this.port);
+        this.cntn = new GenericContainer<>("centos:centos8")
+            .withCommand("tail", "-f", "/dev/null")
+            .withWorkingDirectory("/home/")
+            .withFileSystemBind(this.tmp.toString(), "/home");
+        this.cntn.start();
+        this.cntn.execInContainer("yum", "-y", "install", "maven");
+    }
+
     private String exec(final String... actions) throws Exception {
         return this.cntn.execInContainer(actions).getStdout();
     }
 
-    private List<String> settings() {
-        return new ListOf<String>(
-            "<settings>",
-            "    <profiles>",
-            "        <profile>",
-            "            <id>artipie</id>",
-            "            <repositories>",
-            "                <repository>",
-            "                    <id>my-repo</id>",
-            String.format("<url>http://host.testcontainers.internal:%d/</url>", this.port),
-            "                </repository>",
-            "            </repositories>",
-            "        </profile>",
-            "    </profiles>",
-            "    <activeProfiles>",
-            "        <activeProfile>artipie</activeProfile>",
-            "    </activeProfiles>",
-            "</settings>"
+    private void settings(final Optional<Pair<String, String>> user) throws IOException {
+        final Path setting = this.tmp.resolve("settings.xml");
+        setting.toFile().createNewFile();
+        Files.write(
+            setting,
+            new ListOf<String>(
+                "<settings>",
+                "   <servers>",
+                "       <server>",
+                "           <id>my-repo</id>",
+                user.map(
+                    data -> String.format(
+                        "<username>%s</username>\n<password>%s</password>",
+                        data.getKey(), data.getValue()
+                    )
+                ).orElse(""),
+                "       </server>",
+                "   </servers>",
+                "    <profiles>",
+                "        <profile>",
+                "            <id>artipie</id>",
+                "            <repositories>",
+                "                <repository>",
+                "                    <id>my-repo</id>",
+                String.format("<url>http://host.testcontainers.internal:%d/</url>", this.port),
+                "                </repository>",
+                "            </repositories>",
+                "        </profile>",
+                "    </profiles>",
+                "    <activeProfiles>",
+                "        <activeProfile>artipie</activeProfile>",
+                "    </activeProfiles>",
+                "</settings>"
+            )
         );
     }
 
@@ -190,5 +249,31 @@ public final class MavenITCase {
                     }
             ).value()
         );
+    }
+
+    private Pair<Permissions, Identities> auth(final String type) {
+        final Pair<Permissions, Identities> res;
+        if ("anonymous".equals(type)) {
+            res = new ImmutablePair<>(Permissions.FREE, Identities.ANONYMOUS);
+        } else {
+            res = new ImmutablePair<>(
+                (name, action) -> MavenITCase.USER.getKey().equals(name)
+                    && ("download".equals(action) || "upload".equals(action)),
+                new BasicIdentities(
+                    new Authentication.Single(
+                        MavenITCase.USER.getKey(), MavenITCase.USER.getValue()
+                    )
+                )
+            );
+        }
+        return res;
+    }
+
+    private Optional<Pair<String, String>> getUser(final String type) {
+        Optional<Pair<String, String>> res = Optional.empty();
+        if (!"anonymous".equals(type)) {
+            res = Optional.of(MavenITCase.USER);
+        }
+        return res;
     }
 }
