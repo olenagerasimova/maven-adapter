48b5eff4c6d2717d7b9bddeb8a1921ed3f4214cc
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2020-10-26 15:46:32 +0300

    #219 - Check presence of size in content

--- src/main/java/com/artipie/maven/http/CachedProxySlice.java
+++ src/main/java/com/artipie/maven/http/CachedProxySlice.java
@@ -125,22 +125,31 @@ final class CachedProxySlice implements Slice {
                             .map(CachedProxySlice::checksumControl)
                             .collect(Collectors.toUnmodifiableList())
                         )
-                    ).thenCompose(
-                        pub -> new PublisherAs(pub)
-                            .asciiString()
-                            .thenApply(
-                                str -> {
-                                    final Response resp;
-                                    if (str.contains("404 Not Found")) {
-                                        resp = new RsWithStatus(RsStatus.NOT_FOUND);
-                                    } else {
-                                        resp = new RsWithBody(
-                                            StandardRs.OK, new Content.From(str.getBytes())
-                                        );
-                                    }
-                                    return resp;
-                                }
-                            )
+                    ).thenApply(
+                        pub -> {
+                            final Response rsp;
+                            if (pub.size().isPresent()) {
+                                rsp = new AsyncResponse(new PublisherAs(pub)
+                                    .asciiString()
+                                    .thenApply(
+                                        str -> {
+                                            final Response resp;
+                                            if (str.contains("404 Not Found")) {
+                                                resp = new RsWithStatus(RsStatus.NOT_FOUND);
+                                            } else {
+                                                resp = new RsWithBody(
+                                                    StandardRs.OK, new Content.From(str.getBytes())
+                                                );
+                                            }
+                                            return resp;
+                                        }
+                                    )
+                                );
+                            } else {
+                                rsp = new RsWithBody(StandardRs.OK, new Content.From(pub));
+                            }
+                            return rsp;
+                        }
                     )
             )
         );
